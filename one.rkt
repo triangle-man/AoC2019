fada04f712cbbe81f84e6a26e6e191b74251f4a6
James Geddes <jgeddes@turing.ac.uk>
2019-12-01 10:25:34 +0000

    Day 1.

---
+++ one.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+;; ------------------------------------------------------------
+;; inputs : Listof string?
+(define inputs
+  (with-input-from-file "inputs/one.csv" port->lines))
+
+(define masses (map string->number inputs))
+
+;; ------------------------------------------------------------
+;; Part 1: total fuel
+
+(define (fuel mass)
+  (- (floor (/ mass 3)) 2))
+
+(apply + (map fuel masses))
+
+;; ------------------------------------------------------------
+;; Part 2: with fuel for fuel
+
+;; all-fuels : mass -> mass
+(define (all-fuels mass)
+  (if (<= mass 0)
+      0
+      (+ mass (all-fuels (fuel mass)))))
+
+;; Fuel mass is total mass less mass of modules
+(-
+ (apply + (map all-fuels masses))
+ (apply + masses))
